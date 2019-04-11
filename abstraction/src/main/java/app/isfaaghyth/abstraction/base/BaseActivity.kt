26080e2a647fb360134643816634d93581476d30
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2019-04-11 22:36:22 +0700

    base of classes already prepared

---
+++ abstraction/src/main/java/app/isfaaghyth/abstraction/base/BaseActivity.kt
@@ -0,0 +1,8 @@
+package app.isfaaghyth.abstraction.base
+
+/**
+ * Created by isfaaghyth on 11/04/19.
+ * github: @isfaaghyth
+ */
+class BaseActivity {
+}
---
+++ abstraction/src/main/java/app/isfaaghyth/abstraction/utils/ImageLoader.kt
@@ -0,0 +1,8 @@
+package app.isfaaghyth.abstraction.utils
+
+/**
+ * Created by isfaaghyth on 11/04/19.
+ * github: @isfaaghyth
+ */
+class ImageLoader {
+}
