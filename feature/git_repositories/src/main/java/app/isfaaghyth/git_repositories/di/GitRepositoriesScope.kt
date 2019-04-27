c5d646173f0a6340105a7455e04de6c4d06ac707
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2019-04-27 23:56:00 +0700

    make dagger classes for GitRepositories

---
+++ feature/git_repositories/src/main/java/app/isfaaghyth/git_repositories/di/GitRepositoriesScope.kt
@@ -0,0 +1,10 @@
+package app.isfaaghyth.git_repositories.di
+
+import javax.inject.Scope
+
+/**
+ * Created by isfaaghyth on 27/04/19.
+ * github: @isfaaghyth
+ */
+@Scope
+annotation class GitRepositoriesScope
