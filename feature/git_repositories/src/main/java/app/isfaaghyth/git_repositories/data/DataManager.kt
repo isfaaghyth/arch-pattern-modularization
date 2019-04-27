1319875909e98720b7d249f27bef009d81288ca7
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2019-04-27 23:55:25 +0700

    declaration of data provide of GitRepositories

---
+++ feature/git_repositories/src/main/java/app/isfaaghyth/git_repositories/data/DataManager.kt
@@ -0,0 +1,9 @@
+package app.isfaaghyth.git_repositories.data
+
+import app.isfaaghyth.git_repositories.data.remote.RemoteRepository
+
+/**
+ * Created by isfaaghyth on 27/04/19.
+ * github: @isfaaghyth
+ */
+interface DataManager: RemoteRepository
