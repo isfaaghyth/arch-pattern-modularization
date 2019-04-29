9973e0a0464787a5f64ff3dd6291cd762af324be
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2019-04-29 08:19:02 +0700

    destroy gitRepositories feature module

--- feature/git_repositories/src/main/java/app/isfaaghyth/git_repositories/data/DataManager.kt
+++
@@ -1,9 +0,0 @@
-package app.isfaaghyth.git_repositories.data
-
-import app.isfaaghyth.git_repositories.data.remote.RemoteRepository
-
-/**
- * Created by isfaaghyth on 27/04/19.
- * github: @isfaaghyth
- */
-interface DataManager: RemoteRepository
--- feature/git_repositories/src/main/java/app/isfaaghyth/git_repositories/di/GitRepositoriesScope.kt
+++
@@ -1,9 +0,0 @@
-package app.isfaaghyth.git_repositories.di
-
-import javax.inject.Scope
-
-/**
- * Created by isfaaghyth on 27/04/19.
- * github: @isfaaghyth
- */
-@Scope annotation class GitRepositoriesScope
